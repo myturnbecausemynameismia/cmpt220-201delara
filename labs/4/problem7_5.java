1e4dd285c25ea68f0b854381fd12191ddcce0220
myturnbecausemynameismia <31451257+myturnbecausemynameismia@users.noreply.github.com>
2018-02-16 15:22:01 -0500

    completed 7.5

--- labs/4/problem7_5.java
+++ labs/4/problem7_5.java
@@ -1,4 +1,6 @@
-//This prgram reads 10 given numbers, displays the number of distinct numbers, and displays
+package sd;
+
+//This program reads 10 given numbers, displays the number of distinct numbers, and displays
 	//them in order inputed
 import java.util.Scanner;
 
@@ -8,36 +10,39 @@ public class problem7_5 {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter 10 numbers: ");
 		int[] a;
-		a = new int[10];
+		a = new int[9];
+		
+		int[] nums;
+		nums = new int[9];
+		
 		boolean[] d;
-		d = new boolean[10];
-		
-		int num = input.nextInt();
-		String distinctNum = "";
-		int numOfDistinct = 11;
-		
-		for(int i = 0; i <= 9; i++) {
-			d[i] = true;
+		d = new boolean[9];
+		for(int i = 0; i < 9; i++) {
+			d[i] = false;
 		}
-
 		
-		for(int i = 0; i <= 9; i++) {
-			a[i] = num;
-			String n = a[i] + " ";
-			if(d[i] == true) {
-				distinctNum += n;
-				d[i] = false;
-			}
-			for(int j = 0; j <= 9; j++) {
-				if((i != j) && (num == a[j])) {
-					numOfDistinct -= 1;
-
+		String distinctNum = "";
+		int numOfDistinct = 0;
+		
+		for(int j = 0; j < 9; j++){
+			int num = input.nextInt();
+			nums[j] = num;
+			String n = nums[j] + " ";
+			
+			for(int i = 0; i < 9; i++) {
+				if ((d[i] == true) && (nums[j] == a[i])){
+					break;
+				}
+			
+				else if(d[i]==false){
+					a[i] = nums[j];
+					d[i] = true;
+					distinctNum += n;
+					numOfDistinct += 1;
+					break;
 				}
 			}
-
-			num = input.nextInt();
 		}
-		
 		
 		System.out.println("The number of distinct numbers is " + numOfDistinct);
 		System.out.println("The distinct numbers are: " + distinctNum);
