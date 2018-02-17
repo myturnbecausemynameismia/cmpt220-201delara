7ecf6153fc1d71c94bc2f58df9f9c2c55e0de9ca
myturnbecausemynameismia <31451257+myturnbecausemynameismia@users.noreply.github.com>
2018-02-17 16:17:57 -0500

    project 1 not finished.

---
+++ prj/0/A1_Paper.java
@@ -0,0 +1,153 @@
+package sd;
+import java.util.Scanner;
+
+public class A1_Paper {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		//Enter smallest paper size
+		System.out.print("Enter the A-size of the smallest paper: ");
+		int n = input.nextInt();
+		
+		//check if size is valid
+		if(!checkSize(n)) {
+			System.out.print("impossible");
+			System.exit(1);
+		}
+		
+
+		int[] numOfEach; // array for numOfEach paper size
+		numOfEach = new int[n - 2]; 
+		//^^ An = numOfEach[n - 2]; ex. A2 = numOfEach[0]
+		//Enter number of each paper size
+		System.out.print("Enter the number Bjorn has of each sheet" +
+		"of paper, starting from A2 and ending with A" + n + ": ");
+		//Loop to make numOfEach array values
+		for(int i = 0; i < (numOfEach.length - 1); i++) {
+			numOfEach[i] = input.nextInt();
+		}
+		
+		//check if numOfEach is valid
+		if(!checkNumOfEach(numOfEach)) {
+			System.out.print("impossible");
+			System.exit(1);
+		}
+		
+		//check if A1 is possible
+		if(tapeLength(numOfEach) == 0) {
+			System.out.print("impossibleb");
+			System.exit(1);
+		}
+		
+		else
+			System.out.print(tapeLength(numOfEach));
+		
+		
+		
+	}
+	
+	public static boolean checkSize(int size) {
+		return ((size >= 2) && (size <= 30));
+	}
+	
+	public static Boolean checkNumOfEach(int[] numOfEach) {
+		//reference array, ea. paper can't have more than 10^9 qty
+		int[] ref;
+		ref = new int[28];
+		for(int i = 0; i < 28; i++) {
+			ref[i] = (int) Math.pow(10, 9);
+		}
+		
+		//init boolean result
+		Boolean result = null;
+		
+		//loop determining if numOfEach is valid
+		for(int i = 0; i < (numOfEach.length - 1); i++) {
+			if(numOfEach[i] > ref[i])
+				result = false;
+			else
+				result = true;
+		}
+		return result;
+	}
+	
+	public static float tapeLength(int[] numOfEach) {
+		//2D array to initialize x & y dimensions
+		float[][] dimensions;
+		dimensions = new float[numOfEach.length - 1][1];
+		// ^^ dimensions[i][0] = x
+		// ^^ dimensions[i][1] = y
+		
+		//init tapeLength
+		float tapeLength = 0;
+		
+		// dimensions of A2's x & y
+		float x = (float) Math.pow(2, -(5 / 4));
+		float y = (float) Math.pow(2, -(3 / 4));
+		
+		//init total x & y to compare later
+		float totalX = 0;
+		float totalY = 0;
+		
+		//dimensions of A1's x & y to compare later
+		float a1X = 2 * x;
+		float a1Y = y;
+		
+		//Calculate x & y's for the rest of the sizes
+		for(int i = 2; i < (numOfEach.length - 1); i++) {
+			//checks if dimensions are good
+			if((totalY >= a1Y) && (totalX >= a1X))
+				return tapeLength;
+			//if they have an A2, add tapelength
+			else if(numOfEach[0] > 0) {
+				tapeLength += y;
+				totalY += y * numOfEach[0];
+				totalX += x * numOfEach[0];
+				if(numOfEach[0] >= 2) {
+					tapeLength += y;
+					return tapeLength;
+				}
+			}
+			
+			//check for A3
+			else if(numOfEach[1] > 0) {
+				tapeLength += x;
+				totalX += x * numOfEach[1];
+				totalY += (y / 2) * numOfEach[1];
+				if(numOfEach[1] >= 4) {
+					tapeLength += x + y;
+					return tapeLength;
+				}
+			}
+			else if(i % 2 == 0) {
+				dimensions[i][1] = y / i;
+				tapeLength += dimensions[i][1];
+				totalY += dimensions[i][1];
+				for(int j = numOfEach[i] - 1; j > 0; j--) {
+					if((totalY < a1Y) && (totalX < a1X)) {
+						tapeLength += dimensions[i][1];
+						totalY += dimensions[i][1];
+					}
+				}
+			}
+			else if(i % 2 != 0) {
+				dimensions[i][0] = x / (i + 1);
+				tapeLength += dimensions[i][0];
+				totalX += dimensions[i][0];
+				for(int j = numOfEach[i] - 1; j > 0; j--) {
+					if((totalY < a1Y) && (totalX < a1X)) {
+						tapeLength += dimensions[i][0];
+						totalX += dimensions[i][0];
+					}
+				}
+			}
+		}
+		if((totalY < a1Y) && (totalX < a1X))
+			tapeLength = 0;
+		
+		return tapeLength;
+		
+	}
+	
+}
